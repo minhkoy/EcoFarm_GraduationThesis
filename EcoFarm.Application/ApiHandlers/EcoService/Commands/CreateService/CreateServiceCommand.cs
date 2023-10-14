64ceda4064b39f975a744ff7442ca1e2264cca85
minh <pikoycr2001@gmail.com>
2023-10-14 16:04:20 +0700

    remove create-react-app with javascript

---
+++ EcoFarm.Application/ApiHandlers/EcoService/Commands/CreateService/CreateServiceCommand.cs
@@ -0,0 +1,39 @@
+﻿using EcoFarm.Application.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoFarm.Application.ApiHandlers.EcoService.Commands.CreateService
+{
+    public class CreateServiceCommand : IRequest<CreateServiceResponse>
+    {
+        public string ServiceCode { get; set; }
+        public string ServiceName { get; set; }
+        public string Description { get; set; }
+        public string SupportAccountId { get; set; }
+        public DateTime? StartDate { get; set; }
+    }
+
+    public class CreateServiceResponse
+    {
+
+    }
+
+    internal class CreateServiceHandler : IRequestHandler<CreateServiceCommand, CreateServiceResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        //private readonly 
+        public CreateServiceHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<CreateServiceResponse> Handle(CreateServiceCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
